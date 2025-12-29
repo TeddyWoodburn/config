80f7a60f2b867e2dbc48fae07d2d54f16b18a6b8
TeddyWoodburn <113065068+TeddyWoodburn@users.noreply.github.com>
2025-12-29 22:25:12 +0000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install rsync -y
+sudo apt-get install rsync zsh -y
+chsh -s /usr/bin/zsh
 rsync -ar --delete --info=progress2 td@desktop:/home/td/Documents /home/td/Documents
